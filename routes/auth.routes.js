0350a09ecafefa6c3c76608f077951950dc0327f
CameronStClair95 <cameronaitcheson@gmail.com>
2023-01-17 17:25:51 +0000

    Still working on Lab

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,10 @@ const bcrypt = require('bcryptjs')
 
 const saltRounds = 10
 
+const mongoose = require('mongoose')
+
+const {isLoggedIn, isLoggedOut} = require('../middleware/route-guard.js');
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup')
 });
@@ -32,8 +36,46 @@ router.post('/signup', (req, res) => {
     })
 })
 
-router.get('/profile',(req,res)=>{
-    res.render('user/user-profile')
+router.get('login', isLoggedIn, (req,res) => {
+    console.log(req.session)
+    res.render('auth/login')
 })
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('user/user-profile', {userInfo: req.session.currentUser})
+})
+
+router.post('/login', (req,res) => {
+    console.log("session =====> ", req.session)
+    console.log(req.body)
+    const {username, password} = req.body;
+
+    if (!email || !password) {
+        res.render('auth/login' , {errorMessage:'Please enter an email or password'})
+        return
+    }
+    User.findOne({username})
+    .then(user => {
+        console.log(user)
+        if(!user){
+            res.render('auth/login', {errorMessage: "User not found please sign up. No account associated with username"})
+        } 
+        else if (bcrypt.compareSync(password,user.passwordHash)) {
+            req.session.currentUser = user
+            res.redirect('profile')
+        }
+        else {
+            res.render('auth/login', {errorMessage: "Incorrect Password"})
+        }
+
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+})
+
+// router.get('/profile',(req,res)=>{
+// res.render('user/user-profile')
+// })
 
 module.exports = router
